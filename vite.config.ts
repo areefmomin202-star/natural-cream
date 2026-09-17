3c7bb384df15d79d4b199cad6a05ef881f10736d
areefmomin202-star <areefmomin202@gmail.com>
2026-09-17 20:19:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/natural-cream/',
+    base: '/naturalcreamofficial/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
