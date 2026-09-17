e233a374e157b6d30eeda8b77bee400680784ce3
areefmomin202-star <areefmomin202@gmail.com>
2026-09-17 16:38:55 +0530

    Update productStore.ts

--- data/productStore.ts
+++ data/productStore.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, ReactNode, createElement } from 'react';
 import { ProductConfig } from '../types';
-import { DEFAULT_PRODUCT_CONFIG } from './productData';
+
 
 interface ProductContextType {
   product: ProductConfig;
